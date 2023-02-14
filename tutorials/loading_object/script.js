60cf5491d7abbecb7c796079d0a91b9851741905
Jonas De Schouwer <jonasdeschouwer@gmail.com>
2023-02-14 16:46:16 +0100

    loading object succeeded

--- tutorials/loading_object/script.js
+++ tutorials/loading_object/script.js
@@ -1,6 +1,6 @@
 
 import * as THREE from 'three';
-import { OBJLoader } from '/node_modules/three/addons/loaders/OBJLoader.js'
+import { OBJLoader } from 'three/addons/loaders/OBJLoader.js'
 
 // define scene and camera
 const scene = new THREE.Scene();
@@ -14,7 +14,7 @@ document.body.appendChild( renderer.domElement );
 // load object
 const loader = new OBJLoader();
 loader.load(
-    '../../../assets/lowerjaw.obj',
+    './assets/lowerjaw.obj',
     // called when resource is loaded
     function (object) {
         scene.add(object);
@@ -29,20 +29,17 @@ loader.load(
     }
 );
 
-// // add cube
-// const geometry = new THREE.BoxGeometry( 1, 1, 1 );
-// const material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
-// const cube = new THREE.Mesh( geometry, material );
-// scene.add( cube );
+// add light sources
+const ambientLight = new THREE.AmbientLight(0x404040);
+const directionalLight = new THREE.DirectionalLight(0xffffff, 5);
+scene.add(ambientLight);
+scene.add(directionalLight);
 
-
-camera.position.z = 5;
+camera.position.z = 3;
 
 // render loop
 function animate() {
     requestAnimationFrame( animate );   // tells the browser that we want to run animate again when the screen is refreshed
-    cube.rotation.x += 0.01;
-    cube.rotation.y += 0.01;
     renderer.render( scene, camera );
 }
 animate();
