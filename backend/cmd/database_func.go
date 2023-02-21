0549a30913b2a7d5839ba34c11b9a7d269703f67
DagMalstaf <92030435+DagMalstaf@users.noreply.github.com>
2023-02-21 21:55:37 +0100

    Alter database with functions in main.

--- backend/cmd/database_func.go
+++ backend/cmd/database_func.go
@@ -1,34 +1,5 @@
 package main
 
 import (
-	"database/sql"
-	"fmt"
-
 	_ "github.com/lib/pq"
 )
-
-func addPatient(id int, bite string) error {
-	// Connect to the database
-	db, err := sql.Open("postgres", "host=host.docker.internal port=5432 user=docker password=docker1 dbname=patient_server sslmode=disable")
-	if err != nil {
-		return err
-	}
-	defer db.Close()
-
-	// Prepare a statement with placeholders for the values
-	stmt, err := db.Prepare("INSERT INTO tag (id, bite) VALUES ($1, $2)")
-	if err != nil {
-		return err
-	}
-	defer stmt.Close()
-
-	// Perform database modifications
-	_, err = stmt.Exec(id, bite)
-	if err != nil {
-		return err
-	}
-
-	fmt.Println("Row added successfully")
-	return nil
-
-}
--- backend/cmd/main.go
+++ backend/cmd/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"database/sql"
 	"fmt"
 
 	_ "github.com/lib/pq"
@@ -8,17 +9,17 @@ import (
 
 type Server struct{}
 
-func (s *Server) Scan(
-	ctx context.Context,
-	req *connect.Request[threedoclusionv1.ScanRequest],
-) (*connect.Response[threedoclusionv1.ScanResponse], error) {
-	log.Println("Request headers: ", req.Header())
-	res := connect.NewResponse(&threedoclusionv1.ScanResponse{
-		Name: fmt.Sprintf("Hello, %s!", req.Msg.Id),
-	})
-	res.Header().Set("Scan-Version", "v1")
-	return res, nil
-}
+// func (s *Server) Scan(
+// 	ctx context.Context,
+// 	req *connect.Request[threedoclusionv1.ScanRequest],
+// ) (*connect.Response[threedoclusionv1.ScanResponse], error) {
+// 	log.Println("Request headers: ", req.Header())
+// 	res := connect.NewResponse(&threedoclusionv1.ScanResponse{
+// 		Name: fmt.Sprintf("Hello, %s!", req.Msg.Id),
+// 	})
+// 	res.Header().Set("Scan-Version", "v1")
+// 	return res, nil
+// }
 
 func main() {
 	// server := &Server{}
@@ -31,16 +32,42 @@ func main() {
 	// 	h2c.NewHandler(mux, &http2.Server{}),
 	// )
 
-	id := 123
-	bite := "onderbeet"
+	id := 111
+	bite := "overbeet"
 
 	fmt.Println("starten van toevoeging")
 
-	err := addPatient(id, bite)
+	err := AddPatient(id, bite)
 	if err != nil {
 		fmt.Println(err)
 	}
 
 	fmt.Println("toevoegen succesvol")
+
+}
+
+func AddPatient(id int, bite string) error {
+	// Connect to the database
+	db, err := sql.Open("postgres", "host=host.docker.internal port=5432 user=docker password=docker1 dbname=patient_server sslmode=disable")
+	if err != nil {
+		return err
+	}
+	defer db.Close()
+
+	// Prepare a statement with placeholders for the values
+	stmt, err := db.Prepare("INSERT INTO tag (id, bite) VALUES ($1, $2)")
+	if err != nil {
+		return err
+	}
+	defer stmt.Close()
+
+	// Perform database modifications
+	_, err = stmt.Exec(id, bite)
+	if err != nil {
+		return err
+	}
+
+	fmt.Println("Row added successfully")
+	return nil
 
 }
