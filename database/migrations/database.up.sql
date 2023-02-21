a9b6ca70847b33b5f020f2c1cbf4e972d47981fd
DagMalstaf <92030435+DagMalstaf@users.noreply.github.com>
2023-02-21 16:20:10 +0100

    database basic sql setup

--- database/migrations/database.up.sql
+++ database/migrations/database.up.sql
@@ -2,27 +2,26 @@ CREATE TABLE patient (
     id smallint PRIMARY KEY, 
     name text,
     pinned bit,
-    notes text,);
+    notes text
+);
 
 CREATE TABLE dentist (
     id smallint PRIMARY KEY,
     email text,
     password text,
     fname text,
-    lname text,
+    lname text
 );
 
 CREATE TABLE scan (
     id smallint PRIMARY KEY,
     file text,
-    date date,
+    date date
 );
 
 CREATE TABLE tag (
     id smallint PRIMARY KEY,
-    bite text,
+    bite text
 );
-
-
 
 
