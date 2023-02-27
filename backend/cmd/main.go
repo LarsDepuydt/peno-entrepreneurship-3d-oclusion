f10714013698a68812d39a4e983c1cb1136ff117
DagMalstaf <92030435+DagMalstaf@users.noreply.github.com>
2023-02-27 22:32:56 +0100

    main changes

--- backend/cmd/main.go
+++ backend/cmd/main.go
@@ -1,38 +1,56 @@
 package main
 
 import (
+	"context"
 	"fmt"
+	"log"
+	"net/http"
 
-	//db "dbFunctions"
+	"github.com/bufbuild/connect-go"
+	"github.com/rs/cors"
+	"golang.org/x/net/http2"
+	"golang.org/x/net/http2/h2c"
 
-	_ "github.com/lib/pq"
+	threedoclusionv1 "github.com/LarsDepuydt/peno-entrepreneurship-3d-oclusie/gen/proto/threedoclusion/v1"
+	"github.com/LarsDepuydt/peno-entrepreneurship-3d-oclusie/gen/proto/threedoclusion/v1/threedoclusionv1connect"
 )
 
 type Server struct{}
 
-// func (s *Server) Scan(
-// 	ctx context.Context,
-// 	req *connect.Request[threedoclusionv1.ScanRequest],
-// ) (*connect.Response[threedoclusionv1.ScanResponse], error) {
-// 	log.Println("Request headers: ", req.Header())
-// 	res := connect.NewResponse(&threedoclusionv1.ScanResponse{
-// 		Name: fmt.Sprintf("Hello, %s!", req.Msg.Id),
-// 	})
-// 	res.Header().Set("Scan-Version", "v1")
-// 	return res, nil
-// }
+func (s *Server) Scan(
+	ctx context.Context,
+	req *connect.Request[threedoclusionv1.ScanRequest],
+) (*connect.Response[threedoclusionv1.ScanResponse], error) {
+	log.Println("Request headers: ", req.Header())
+	res := connect.NewResponse(&threedoclusionv1.ScanResponse{
+		Name: fmt.Sprintf("Hello, %s!", req.Msg.Id),
+	})
+
+	res.Header().Set("Scan-Version", "v1")
+	return res, nil
+}
 
 func main() {
-	fmt.Println("starten van main")
-	// server := &Server{}
-	// mux := http.NewServeMux()
-	// path, handler := threedoclusionv1connect.NewScanServiceHandler(server)
-	// mux.Handle(path, handler)
-	// http.ListenAndServe(
-	// 	"0.0.0.0:8080",
-	// 	// Use h2c so we can serve HTTP/2 without TLS.
-	// 	h2c.NewHandler(mux, &http2.Server{}),
-	// )
+	server := &Server{}
+
+	mux := http.NewServeMux()
+	path, handler := threedoclusionv1connect.NewScanServiceHandler(server)
+	mux.Handle(path, handler)
+
+	muxHandler := cors.Default().Handler(mux)
+	c := cors.New(cors.Options{
+		AllowedOrigins:   []string{"*"},
+		AllowCredentials: true,
+		AllowedHeaders:   []string{"Connect-Protocol-Version", "Content-Type"},
+		Debug:            true, // Enable Debugging for testing, consider disabling in production
+	})
+	muxHandler = c.Handler(muxHandler)
+
+	http.ListenAndServe(
+		"0.0.0.0:8080",
+		// Use h2c so we can serve HTTP/2 without TLS.
+		h2c.NewHandler(muxHandler, &http2.Server{}),
+	)
 
 	id := 111
 	bite := "overbeet"
