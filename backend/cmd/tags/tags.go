8ac95d3f2c6bb85e1969b8a06538942ae82043b0
Lars Depuydt <lars.depuydt@gmail.com>
2023-02-28 19:46:54 +0100

    fix `oclusie` mistake

--- backend/cmd/tags/tags.go
+++ backend/cmd/tags/tags.go
@@ -5,7 +5,7 @@ import (
 
 	"github.com/bufbuild/connect-go"
 
-	threedoclusionv1 "github.com/LarsDepuydt/peno-entrepreneurship-3d-oclusie/gen/proto/threedoclusion/v1"
+	threedoclusionv1 "github.com/LarsDepuydt/peno-entrepreneurship-3d-oclusion/gen/proto/threedoclusion/v1"
 )
 
 
