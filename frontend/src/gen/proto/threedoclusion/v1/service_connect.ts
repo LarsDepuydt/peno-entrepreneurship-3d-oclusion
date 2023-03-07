1323e1e075e12b98fd72f93a644b6a05d40fdd74
DagMalstaf <92030435+DagMalstaf@users.noreply.github.com>
2023-03-07 17:54:03 +0100

    Adding Patient functions

--- frontend/src/gen/proto/threedoclusion/v1/service_connect.ts
+++ frontend/src/gen/proto/threedoclusion/v1/service_connect.ts
@@ -3,7 +3,7 @@
 /* eslint-disable */
 // @ts-nocheck
 
-import { AddScanRequest, AddScanResponse, AddTagRequest, AddTagResponse, DeleteScanRequest, DeleteScanResponse, DeleteTagRequest, DeleteTagResponse, GetAllScansRequest, GetAllScansResponse, GetAllTagsByTypeRequest, GetAllTagsByTypeResponse, GetAllTagsRequest, GetAllTagsResponse, GetScanByDateRequest, GetScanByDateResponse, GetScanByIDRequest, GetScanByIDResponse, GetTagByIDRequest, GetTagByIDResponse } from "./service_pb.js";
+import { AddPatientRequest, AddPatientResponse, AddScanRequest, AddScanResponse, AddTagRequest, AddTagResponse, DeletePatientRequest, DeletePatientResponse, DeleteScanRequest, DeleteScanResponse, DeleteTagRequest, DeleteTagResponse, GetAllPatientsRequest, GetAllPatientsResponse, GetAllScansRequest, GetAllScansResponse, GetAllTagsByTypeRequest, GetAllTagsByTypeResponse, GetAllTagsRequest, GetAllTagsResponse, GetPatientByIDRequest, GetPatientByIDResponse, GetPatientByNameRequest, GetPatientByNameResponse, GetScanByDateRequest, GetScanByDateResponse, GetScanByIDRequest, GetScanByIDResponse, GetTagByIDRequest, GetTagByIDResponse } from "./service_pb.js";
 import { MethodKind } from "@bufbuild/protobuf";
 
 /**
@@ -100,6 +100,51 @@ export const ScanService = {
       name: "GetAllTagsByType",
       I: GetAllTagsByTypeRequest,
       O: GetAllTagsByTypeResponse,
+      kind: MethodKind.Unary,
+    },
+    /**
+     * @generated from rpc threedoclusion.v1.ScanService.AddPatient
+     */
+    addPatient: {
+      name: "AddPatient",
+      I: AddPatientRequest,
+      O: AddPatientResponse,
+      kind: MethodKind.Unary,
+    },
+    /**
+     * @generated from rpc threedoclusion.v1.ScanService.DeletePatient
+     */
+    deletePatient: {
+      name: "DeletePatient",
+      I: DeletePatientRequest,
+      O: DeletePatientResponse,
+      kind: MethodKind.Unary,
+    },
+    /**
+     * @generated from rpc threedoclusion.v1.ScanService.GetAllPatients
+     */
+    getAllPatients: {
+      name: "GetAllPatients",
+      I: GetAllPatientsRequest,
+      O: GetAllPatientsResponse,
+      kind: MethodKind.Unary,
+    },
+    /**
+     * @generated from rpc threedoclusion.v1.ScanService.GetPatientByID
+     */
+    getPatientByID: {
+      name: "GetPatientByID",
+      I: GetPatientByIDRequest,
+      O: GetPatientByIDResponse,
+      kind: MethodKind.Unary,
+    },
+    /**
+     * @generated from rpc threedoclusion.v1.ScanService.GetPatientByName
+     */
+    getPatientByName: {
+      name: "GetPatientByName",
+      I: GetPatientByNameRequest,
+      O: GetPatientByNameResponse,
       kind: MethodKind.Unary,
     },
   }
