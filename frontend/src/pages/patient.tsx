543c3e7103a694382e7797936bf58adaf287b7d5
Astrid Dewever <100223862+astriddewever@users.noreply.github.com>
2023-03-08 09:33:53 +0100

    Patient gallery with variable patients and display of patient name (#7)

--- frontend/src/pages/patient.tsx
+++ frontend/src/pages/patient.tsx
@@ -1,14 +1,18 @@
 import type { AppProps } from 'next/app'
 import 'bootstrap/dist/css/bootstrap.css'
 
-import AddNew_Patient from '../components/popups/new-patient'
-import PatientGallery_Temp from '../components/image-button'
+import New_Patient from '../components/popups/new-patient'
+import {SinglePatient as Patient} from '../components/patient/patient-individual-overview'
+import teeth3d from '../../public/3d-teeth.jpg'
+
 
 export default function PatientPage({ Component, pageProps }: AppProps) {
   return (
     <>
-    <PatientGallery_Temp/>
-    <AddNew_Patient/>
+    <Patient picture={teeth3d} patientfirstname={'Jos'} patientlastname={'Van de Velde'}/>
+    <Patient picture={teeth3d} patientfirstname={'Ann'} patientlastname={'Janssens'}/>
+    <Patient picture={teeth3d} patientfirstname={'Yolande'} patientlastname={'Berbers'}/>
+    <New_Patient/>
     </>
   ) 
 }
