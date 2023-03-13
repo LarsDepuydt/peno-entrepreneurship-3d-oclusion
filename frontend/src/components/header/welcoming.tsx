f80f2355e0e3bc5782b663644162a09fdcfdb8be
Anna Proost <anna.proost@student.kuleuven.be>
2023-03-13 20:52:35 +0100

    add tags + logout button

---
+++ frontend/src/components/header/welcoming.tsx
@@ -0,0 +1,14 @@
+// possibly welcome doctor in the future ?
+
+export default function Welcoming(doctor_name: string) { 
+
+    const Welcoming = (doctor_name: string) => {
+        const welcome = "Welcome " + doctor_name 
+        return welcome
+    }
+    
+    return (
+    <>
+    <h2>Welcoming(doctor_name)</h2>
+    </> )
+  }
--- frontend/src/pages/patient.tsx
+++ frontend/src/pages/patient.tsx
@@ -1,12 +1,14 @@
 import type { AppProps } from 'next/app'
 import 'bootstrap/dist/css/bootstrap.css'
 
+import Header from '../components/header/header'
 import AddNew_Patient from '../components/popups/new-patient'
 import PatientGallery_Temp from '../components/image-button'
 
 export default function PatientPage({ Component, pageProps }: AppProps) {
   return (
     <>
+    <Header/>
     <PatientGallery_Temp/>
     <AddNew_Patient/>
     </>
