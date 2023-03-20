e201c39764d82f6439693f4466ca33e944376181
ArneVanDamme <arne@van-damme.eu>
2023-03-20 09:20:58 +0100

    css gallery page

--- frontend/src/pages/patient.tsx
+++ frontend/src/pages/patient.tsx
@@ -1,15 +1,14 @@
-import type { AppProps } from 'next/app'
-import 'bootstrap/dist/css/bootstrap.css'
-import styles from '@/styles/PatientPage.module.css'
+import type { AppProps } from 'next/app';
+import 'bootstrap/dist/css/bootstrap.css';
+import styles from '@/styles/PatientPage.module.css';
 
-import Header from '../components/header/header'
-import New_Patient from '../components/popups/new-patient'
-import {SinglePatient as Patient} from '../components/patient/patient-individual-overview'
-import teeth3d from '../../public/3d-teeth.jpg'
+import Header from '../components/header/header';
+import { SinglePatient as Patient } from '../components/patient/patient-individual-overview';
+import teeth3d from '../../public/3d-teeth.jpg';
 import { useRouter } from 'next/router';
-import { StaticImageData } from 'next/image'
-import {FC} from 'react';
-
+import { StaticImageData } from 'next/image';
+import { FC } from 'react';
+import Sidebar from '../components/header/sidebar';
 
 interface TableProps {
   data: { [key: string]: string }[];
@@ -17,9 +16,8 @@ interface TableProps {
 
 const Table: FC<TableProps> = ({ data }) => {
   return (
-    <table>
-      <thead>
-      </thead>
+    <table className={styles.patientscanTable}>
+      <thead></thead>
       <tbody>
         {data.map((row, index) => (
           <tr key={index}>
@@ -33,23 +31,44 @@ const Table: FC<TableProps> = ({ data }) => {
   );
 };
 
-
 const patients = [
-  { patient1 : <Patient picture={teeth3d} patientfirstname={'Jos'} patientlastname={'Van de Velde'}/> , patient2: <Patient picture={teeth3d} patientfirstname={'Jos'} patientlastname={'Van de Velde'}/>, patient3: <Patient picture={teeth3d} patientfirstname={'Jos'} patientlastname={'Van de Velde'}/> },
-  { patient1 : <Patient picture={teeth3d} patientfirstname={'Jos'} patientlastname={'Van de Velde'}/> , patient2: <Patient picture={teeth3d} patientfirstname={'Jos'} patientlastname={'Van de Velde'}/>, patient3: <Patient picture={teeth3d} patientfirstname={'Jos'} patientlastname={'Van de Velde'}/> },
-  { patient1 : <Patient picture={teeth3d} patientfirstname={'Jos'} patientlastname={'Van de Velde'}/> , patient2: <Patient picture={teeth3d} patientfirstname={'Jos'} patientlastname={'Van de Velde'}/>, patient3: <Patient picture={teeth3d} patientfirstname={'Jos'} patientlastname={'Van de Velde'}/> },
+  {
+    patient1: <Patient picture={teeth3d} patientfirstname={'jos'} patientlastname={'Van de Velde'} />,
+    patient2: <Patient picture={teeth3d} patientfirstname={'Jos'} patientlastname={'Van de Velde'} />,
+    patient3: <Patient picture={teeth3d} patientfirstname={'Jos'} patientlastname={'Van de Velde'} />,
+  },
+  {
+    patient1: <Patient picture={teeth3d} patientfirstname={'Jos'} patientlastname={'Van de Velde'} />,
+    patient2: <Patient picture={teeth3d} patientfirstname={'Jos'} patientlastname={'Van de Velde'} />,
+    patient3: <Patient picture={teeth3d} patientfirstname={'Jos'} patientlastname={'Van de Velde'} />,
+  },
+  {
+    patient1: <Patient picture={teeth3d} patientfirstname={'Jos'} patientlastname={'Van de Velde'} />,
+    patient2: <Patient picture={teeth3d} patientfirstname={'Jos'} patientlastname={'Van de Velde'} />,
+    patient3: <Patient picture={teeth3d} patientfirstname={'Jos'} patientlastname={'Van de Velde'} />,
+  },
+  {
+    patient1: <Patient picture={teeth3d} patientfirstname={'Jos'} patientlastname={'Van de Velde'} />,
+    patient2: <Patient picture={teeth3d} patientfirstname={'Jos'} patientlastname={'Van de Velde'} />,
+    patient3: <Patient picture={teeth3d} patientfirstname={'Jos'} patientlastname={'Van de Velde'} />,
+  },
+  {
+    patient1: <Patient picture={teeth3d} patientfirstname={'Jos'} patientlastname={'Van de Velde'} />,
+    patient2: <Patient picture={teeth3d} patientfirstname={'Jos'} patientlastname={'Van de Velde'} />,
+  },
 ];
 
 const App: FC = () => {
   return (
     <div>
-      <Header/>
-      <h1> This is your patient overview</h1>
+      <Header />
+      <Sidebar />
+      <div className={styles.textWrapper}>
+        <h1 className={styles.bigText}> Patient Overview</h1>
+      </div>
       <Table data={patients} />
-      <New_Patient/>
     </div>
   );
 };
 
 export default App;
-
