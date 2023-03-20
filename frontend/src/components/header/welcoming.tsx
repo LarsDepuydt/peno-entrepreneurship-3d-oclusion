f1fe282dab8274250d98aafcc05c789af3c02a9a
Astrid Dewever <100223862+astriddewever@users.noreply.github.com>
2023-03-20 10:21:41 +0100

    Layout + vorig donderdag (#25)
    * Hotfix 
    
    Deletion of } accidentally
    
    * header multipurpose
    
    Changing header to multipurpose for patient gallery and also for scans gallery. In patient gallery in the header is showed: "Welcome Dr...". In the Scan Gallery is showed: "Scans of patient Firstname Lastname under supervision of Dr .. "
    
    * patient redirection to scan gallery page completed
    
    In the patient Gallery page, when clicking on the Image of a certain patient. It sends to the scans gallery page. in the URL is the first name and last name of the patient passed.

--- frontend/src/components/header/welcoming.tsx
+++ frontend/src/components/header/welcoming.tsx
@@ -1,20 +1,36 @@
 // possibly welcome doctor in the future ?
 import styles from '@/styles/PatientPage.module.css';
 
-interface Greeting {
-  doctorfirstname: string;
-  doctorlastname: string;
+interface GreetingDoctor {
+    doctorfirstname : string
+    doctorlastname : string
 }
 
-export default function Welcoming({ doctorfirstname, doctorlastname }: Greeting) {
-  const Welcoming = (doctor_name: string) => {
-    const welcome = 'Welcome ' + doctor_name;
-    return welcome;
-  };
+export function WelcomingDoctor({doctorfirstname, doctorlastname}: GreetingDoctor) { 
 
-  return (
+    const Welcoming = (doctor_name: string) => {
+        const welcome = "Welcome " + doctor_name 
+        return welcome
+    }
+    return (
     <>
-      <h2 className={styles.welcoming}>{'Welcome Dr. '.concat(doctorfirstname, ' ', doctorlastname)}</h2>
+        <h2>{("Welcome Dr. ").concat(doctorfirstname, ' ', doctorlastname)}</h2>
+    </> )
+  }
+
+
+interface GreetingPatient {
+    patientfirstname : string 
+    patientlastname : string
+    doctorfirstname : string
+    doctorlastname : string
+}
+
+export function WelcomingPatient({patientfirstname, patientlastname, doctorfirstname, doctorlastname}: GreetingPatient){
+    return (
+    <>
+        <h3>{("Scans of patient ").concat(patientfirstname, ' ', patientlastname, '\n')}</h3>
+        <h4>{("under supervision of Dr. ").concat(doctorfirstname, " ", doctorlastname)} </h4>
     </>
-  );
+        )
 }
