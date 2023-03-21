7704fd35e6dc113f45ebc59dd4a169dcb123a36f
DagMalstaf <92030435+DagMalstaf@users.noreply.github.com>
2023-03-21 16:08:28 +0100

    added foreign keys

--- database/migrations/database.up.sql
+++ database/migrations/database.up.sql
@@ -3,7 +3,8 @@ CREATE TABLE patient (
     first_name text,
     last_name text,
     pinned bit,
-    notes text
+    notes text,
+    dentist_id SERIAL REFERENCES dentist(id)
 );
 
 CREATE TABLE dentist (
@@ -11,17 +12,21 @@ CREATE TABLE dentist (
     email text,
     pass_word text,
     first_name text,
-    last_name text
+    last_name text,
+    patient_id SERIAL REFERENCES patient(id)
 );
 
 CREATE TABLE scan (
     id SERIAL PRIMARY KEY,
     scan text,
-    date_scan date
+    date_scan date,
+    patient_id SERIAL REFERENCES patient(id)
 );
 
 CREATE TABLE tag (
     id SERIAL PRIMARY KEY,
-    bite text
+    bite text,
+    scan_id SERIAL REFERENCES scan(id),
+    patient_id SERIAL REFERENCES patient(id)
 );
 
