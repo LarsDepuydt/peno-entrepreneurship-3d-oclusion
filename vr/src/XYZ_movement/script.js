7d5abd2c4458e647e40cb00fd7e4d09ed7714a60
kjell mesotten <kjell.mesotten@student.kuleuven.be>
2023-03-21 18:36:29 +0100

    not working

--- vr/src/XYZ_movement/script.js
+++ vr/src/XYZ_movement/script.js
@@ -7,8 +7,16 @@ import * as CANNON from 'cannon-es';
 import { MeshBVH, acceleratedRaycast } from 'three-mesh-bvh';
 import { default as CannonUtils } from 'cannon-utils';
 
+//added
+let meshtest;
+//var constrained;
+//
+
+const intersected = [];
 let container;
 let camera, scene, renderer;
+
+// controller1 = rechts, 2 = links
 let controller1, controller2;
 let controllerGrip1, controllerGrip2;
 
@@ -18,9 +26,12 @@ let raycaster;
 
 let world, timeStep=1/10;
 
+const tempMatrix = new THREE.Matrix4();
 
 let meshes = [], bodies = [];
 let floor_shape, floor_body;
+
+let controllerbody1, controllerbody2;
 
 // lj_group is een THREE.Group < THREE.Object3D
 // lj_mesh is een THREE.Mesh
@@ -50,6 +61,11 @@ function initCannon() {
     uj_body.position.set(0,0,200);
     lj_body.quaternion = new CANNON.Quaternion(0, 0, 0, 1);
     uj_body.quaternion = new CANNON.Quaternion(0, 0, 0, 1);
+
+    //added
+    world.quaternion = new CANNON.Quaternion(0, 0, 0, 1);
+    //
+
     world.addBody(lj_body);
     world.addBody(uj_body);
     bodies.push(lj_body);
@@ -67,6 +83,25 @@ function initCannon() {
     floor_body.addShape(floor_shape);
     // floor_body.quaternion.setFromAxisAngle(new CANNON.Vec3(1,0,0),-Math.PI/2);
     //world.addBody(floor_body);
+
+    //initialize controllers in cannon-es
+    // Create a shape for the VR controller
+    var shape = new CANNON.Sphere(0.1);
+
+    // Create a rigid body for the VR controller
+    controllerbody1 = new CANNON.Body({
+        mass: 1,
+        shape: shape,
+    });
+
+    controllerbody2 = new CANNON.Body({
+        mass: 1,
+        shape: shape,
+    });
+
+    // Add the rigid controller bodies to the physics world
+    world.addBody(controllerbody1);
+    world.addBody(controllerbody2);
 }
 
 
@@ -149,9 +184,20 @@ function initThree() {
     // controllers
 
     controller1 = renderer.xr.getController( 0 );
+
+    //added
+    //controller1.quaternion = new CANNON.Quaternion(0, 0, 0, 1);
+    controller1.addEventListener( 'selectstart', onSelectStart );
+    controller1.addEventListener( 'selectend', onSelectEnd );
+    //
     scene.add( controller1 );
 
     controller2 = renderer.xr.getController( 1 );
+    //added
+    //controller2.quaternion = new CANNON.Quaternion(0, 0, 0, 1);
+    controller2.addEventListener( 'selectstart', onSelectStart );
+    controller2.addEventListener( 'selectend', onSelectEnd );
+    //
     scene.add( controller2 );
 
 
@@ -170,8 +216,29 @@ function initThree() {
 
     // lines pointing from controllers
 
+    const geometry = new THREE.BufferGeometry().setFromPoints( [ new THREE.Vector3( 0, 0, 0 ), new THREE.Vector3( 0, 0, - 1 ) ] );
+
+    const line = new THREE.Line( geometry );
+    line.name = 'line';
+    line.scale.z = 5;
+
+    controller1.add( line.clone() );
+    controller2.add( line.clone() );
+
     raycaster = new THREE.Raycaster();
 
+    //added for testing
+    // Create a Three.js geometry for the mesh
+    const testgeo = new THREE.SphereGeometry(1, 1, 1);
+
+    // Create a Three.js material for the mesh
+    const materialtest = new THREE.MeshBasicMaterial({ color: 0xff0000 });
+
+    // Create a Three.js mesh and add it to the scene
+    meshtest = new THREE.Mesh(testgeo, materialtest);
+    meshtest.scale.multiplyScalar(0.5);
+    scene.add(meshtest);
+    
 
     // resize
 
@@ -179,22 +246,94 @@ function initThree() {
 
 }
 
+/*//added
+// Update the position and orientation of the rigid body based on the VR controller
+renderer.xr.addEventListener("frame", function (event) {
+    controllerbody1.position.copy(controller1.position);
+    controllerbody1.quaternion.copy(controller1.quaternion);
+
+    controllerbody2.position.copy(controller2.position);
+    controllerbody2.quaternion.copy(controller2.quaternion);
+  });
+  
+  // Copy the position and orientation of the rigid body back to the VR controller in the three.js world
+  renderer.xr.addEventListener("frame", function (event) {
+    controller1.position.copy(controllerbody1.position);
+    controller1.quaternion.copy(controllerbody1.quaternion);
+
+    controller2.position.copy(controllerbody2.position);
+    controller2.quaternion.copy(controllerbody2.quaternion);
+  });  */
+
+
 // when controller pushes select button, select the object it is pointing to
 
-function onSelectStart( event ) {
+function onSelectStartold( event ) {
 
     const controller = event.target;
 
     const intersections = getIntersections( controller );
-
+    console.log(intersections);
     if ( intersections.length > 0 ) {
 
         const intersection = intersections[ 0 ];
 
         const object = intersection.object;
         object.material.emissive.b = 1;
+        
+        console.log(object);
         //controller.attach( object );
-        addConstraintToBody(controller, object);
+        console.log(object);
+        console.log(uj_body);
+
+        controller.userData.selected = object;
+
+    }
+
+}
+
+
+
+function onSelectStart( event ) {
+
+    const controller = event.target;
+    //const controller = controllerbody1;
+    //const intersections = getIntersections( controllerbody1 );
+    const intersections = getIntersections( controller );
+
+    if ( intersections.length > 0 ) {
+
+        const intersection = intersections[ 0 ];
+
+        var object = intersection.object;
+        var controllerbody;
+
+        object.material.emissive.b = 1;
+        //controller.attach( object );
+
+        //console.log(object);
+        if (object.uuid == lj_mesh.uuid) {
+            object = lj_body;
+        }
+        if (object.uuid == uj_mesh.uuid) {
+            object = uj_body;
+        }
+
+        if (controller.uuid == controller1.uuid) {
+            controllerbody = controllerbody1;
+        }
+        if (controller.uuid == controller2.uuid) {
+            controllerbody = controllerbody2;
+        }
+
+        //addConstraintToBody(controller, object);
+        //console.log(object);
+        //console.log(uj_mesh);
+        //console.log(uj_body);
+        //object = uj_body;
+        //console.log(controllerbody.uuid == controllerbody1.uuid);
+        //console.log(object.uuid == uj_body.uuid);
+        addConstraintToBody(controllerbody, object); 
 
         controller.userData.selected = object;
 
@@ -222,7 +361,7 @@ function onSelectEnd( event ) {
 }
 
 // Based on the dragging_teeth version but now for cannon.js, could be completely wrong
-function getIntersections(controller, world) {
+/*function getIntersections(controller) {
     const ray = new CANNON.Ray();
     const origin = new CANNON.Vec3();
     const direction = new CANNON.Vec3();
@@ -230,22 +369,74 @@ function getIntersections(controller, world) {
     // Set the origin of the ray to the controller's position in the world
     origin.copy(controller.position);
     world.quaternion.vmult(origin, origin); // transform from local to world space
-  
+
     // Set the direction of the ray to be pointing straight ahead from the controller
     direction.set(0, 0, -1);
     controller.quaternion.vmult(direction, direction); // transform from local to world space
-  
-    ray.origin.copy(origin);
-    ray.direction.copy(direction);
+
+    //ray.origin.copy(origin);
+    //ray.direction.copy(direction);
+    ray.from.copy(origin);
+    ray.to.copy(origin).vadd(direction);
+
   
     // intersect with all bodies in the world
     const result = new CANNON.RaycastResult();
+    
     world.raycastAll(ray, result);
-  
-    return result.hasHit ? result : null;
-  }
+    console.log(result)
 
-function intersectObjects( controller, intersected ) {
+    if (result.length > 0) {
+        // return the first RaycastResult in the array
+        return result.allHits[0];
+    } else {
+        return null;
+    }
+}*/
+
+function getIntersections( controller ) {
+
+    tempMatrix.identity().extractRotation( controller.matrixWorld );
+
+    raycaster.ray.origin.setFromMatrixPosition( controller.matrixWorld );
+    raycaster.ray.direction.set( 0, 0, - 1 ).applyMatrix4( tempMatrix );
+
+    //console.log([lj_group, uj_group]);
+    return raycaster.intersectObjects( [lj_group, uj_group], true );
+
+}
+
+// highlight the object the controller points at
+
+function intersectObjects( controller ) {
+
+    // Do not highlight when already selected
+
+    if ( controller.userData.selected !== undefined ) return;
+
+    const line = controller.getObjectByName( 'line' );
+    const intersections = getIntersections( controller );
+
+    if ( intersections.length > 0 ) {
+
+        const intersection = intersections[ 0 ];
+
+        const object = intersection.object;
+        object.material.emissive.r = 1;
+        intersected.push( object );
+
+        line.scale.z = intersection.distance;
+
+    } else {
+
+        line.scale.z = 5;
+
+    }
+
+}
+
+
+/*function intersectObjectsnew( controller, intersected ) {
 
     // Do not highlight when already selected
 
@@ -278,19 +469,27 @@ function intersectObjects( controller, intersected ) {
 
     }
 
-}
+}*/
 
 function addConstraintToBody(body, target) {
+    // Set the pivot points to the center of mass of each body
+    const pivotA = new CANNON.Vec3().copy(body.position);
+    const pivotB = new CANNON.Vec3().copy(target.position);
+
     const constraint = new CANNON.LockConstraint(body, target, {
-      maxForce: 1000,
+      maxForce: 1000000000000000000000000,
       collideConnected: false,
       wakeUpBodies: true,
       wakeUpA: true,
       wakeUpB: true,
-      pivotA: new CANNON.Vec3(),
-      pivotB: new CANNON.Vec3(),
+      //pivotA: new CANNON.Vec3(),
+      //pivotB: new CANNON.Vec3(),
+      pivotA: pivotA,
+      pivotB: pivotB,
     });
     world.addConstraint(constraint);
+    console.log(world.constraints);
+    //constrained = true;
     return constraint;
 }
   
@@ -303,7 +502,7 @@ function removeConstraintFromBody(body) {
     }
 }
 
-controller1.addEventListener("selectstart", (event) => {
+/*controller1.addEventListener("selectstart", (event) => {
     lj_sphere.visible = true;
     lj_sphere.position.copy(controller1.position);
     addConstraintToBody(lj_body, lj_sphere, lj_sphere.position);
@@ -312,8 +511,7 @@ controller1.addEventListener("selectstart", (event) => {
 controller1.addEventListener("selectend", (event) => {
     lj_sphere.visible = false;
     removeConstraintFromBody(lj_body);
-});
-
+});*/
 
 function showAxes() {
     // Create the axes
@@ -378,6 +576,7 @@ function loadObjects() {
             
             lj_mesh = getFirstMesh(lj_group);
             //console.log(lj_mesh);
+            //lj_mesh.geometry.scale(0.01, 0.01, 0.01);
             lj_shape = threeMeshToCannonMesh(lj_mesh);
             console.log("loading lj_group succeeded");
             lj_body.addShape(lj_shape);
@@ -496,8 +695,28 @@ function updatePhysics() {
     uj_mesh.position.copy(uj_body.position);
     uj_mesh.quaternion.copy(uj_body.quaternion);
     uj_sphere.position.copy(uj_body.position);
+
+
+    //added
+    // copy coordinates from three.js to cannon for controller
+    controllerbody1.position.copy(controller1.position);
+    controllerbody2.position.copy(controller2.position);
+    controllerbody1.quaternion.copy(controller1.quaternion);
+    controllerbody2.quaternion.copy(controller2.quaternion);
+    meshtest.position.copy(controllerbody1.position);
+    //
 }
 
+function cleanIntersected() {
+
+    while ( intersected.length ) {
+
+        const object = intersected.pop();
+        object.material.emissive.r = 0;
+
+    }
+
+}
 
 function animate() {
 
@@ -507,12 +726,13 @@ function animate() {
 }
 
 function render() {
-    //cleanIntersected();
+    cleanIntersected();
 
-    //intersectObjects( controller1 );
-    //intersectObjects( controller2 );
+    intersectObjects( controller1 );
+    intersectObjects( controller2 );
     renderer.render( scene, camera );
 }
+
 
 
 function startAnimation() {
