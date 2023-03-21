0140853fc38e5ede603ac720662a2249891b0d5d
Jonas De Schouwer <jonasdeschouwer@gmail.com>
2023-03-21 16:38:54 +0100

    Collision detection & handling working, but not performant. Hence replaced the current teeth models by a gourd and a cube

--- vr/src/cannon_collision/script.js
+++ vr/src/cannon_collision/script.js
@@ -48,16 +48,18 @@ loadObjects();  // animation is started after both objects are loaded
 
 function initCannon() {
     world = new CANNON.World();
-    world.gravity.set(0,-0.4,0);
+    world.gravity.set(0,-0.02,0);
     world.broadphase = new CANNON.NaiveBroadphase();
-    world.solver.iterations = 10;
+    world.broadphase.useBoundingBoxes = true;
+    world.solver.iterations = 4;     //10
 
     lj_body = new CANNON.Body({mass: 1});
     uj_body = new CANNON.Body({mass: 1});
     lj_body.position.set(0,2,0);
-    uj_body.position.set(0,2,0);
-    lj_body.quaternion = new CANNON.Quaternion(0, 0, 0, 1);
-    uj_body.quaternion = new CANNON.Quaternion(0, 0, 0, 1);
+    uj_body.position.set(0,4,0);
+    let xaxis = new CANNON.Vec3(1,0,0);
+    lj_body.quaternion.setFromAxisAngle(xaxis, -Math.PI/2);
+    uj_body.quaternion.setFromAxisAngle(xaxis, -Math.PI/2);
     world.addBody(lj_body);
     world.addBody(uj_body);
     // lj_body.addEventListener("collide", function(e) {
@@ -190,13 +192,13 @@ function loadObjects() {
     // load lower jaw
     const loader = new OBJLoader();
     loader.load(
-        '../../assets/lower_ios_6.obj',
+        '../../assets/random_objects/cube.obj',
+        //'../../assets/lower_ios_6.obj'
         // called when resource is loaded y=green, x=red, z=blue
         function (object) {         // object is a 'Group', which is a subclass of 'Object3D'
             lj_mesh = getFirstMesh(object);
             
-            //lj_group.scale.set(0.01, 0.01, 0.01);
-            lj_mesh.geometry.scale(0.01, 0.01, 0.01);
+            // lj_mesh.geometry.scale(0.01, 0.01, 0.01);
             lj_mesh.position.x = 0;
             lj_mesh.position.y = 0;
             lj_mesh.position.z = 0;
@@ -205,7 +207,7 @@ function loadObjects() {
             console.log(lj_mesh);
             scene.add(lj_mesh);
             
-            lj_shape = threeMeshToCannonMesh(lj_mesh);
+            lj_shape = threeMeshToConvexCannonMesh(lj_mesh);
             console.log("loading lj_mesh succeeded");
             lj_body.addShape(lj_shape);
             lj_loaded = true;
@@ -224,13 +226,12 @@ function loadObjects() {
  
     // load upper jaw
     loader.load(
-        '../../assets/upper_ios_6.obj',
+        '../../assets/random_objects/gourd.obj',
         // called when resource is loaded y=green, x=red, z=blue
         function (object) {
             uj_mesh = getFirstMesh(object);
             
-            //uj_group.scale.set(0.01, 0.01, 0.01);
-            uj_mesh.geometry.scale(0.01, 0.01, 0.01);
+            // uj_mesh.geometry.scale(0.01, 0.01, 0.01);
             uj_mesh.position.x = 0;
             uj_mesh.position.y = 0;
             uj_mesh.position.z = 0;
@@ -239,7 +240,7 @@ function loadObjects() {
             console.log(uj_mesh);
             scene.add(uj_mesh);
             
-            uj_shape = threeMeshToCannonMesh(uj_mesh);
+            uj_shape = threeMeshToConvexCannonMesh(uj_mesh);
             console.log("loading uj_mesh succeeded");
             uj_body.addShape(uj_shape);
             uj_loaded = true;
@@ -378,7 +379,7 @@ function updatePhysics() {
 
 function animate() {
 
-    // console.log("frame", frameNum);
+    console.log("frame", frameNum);
     frameNum += 1;
     updatePhysics();
     render();
