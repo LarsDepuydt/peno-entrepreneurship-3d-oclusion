312bff3d6a26c05e07c656a932e0839cfbee46c4
anna-proost <94004210+anna-proost@users.noreply.github.com>
2023-03-21 14:53:04 +0100

    Frontend/fileupload (#32)
    * fixed merge problems
    
    * working on uploading scans
    
    * file upload tsx
    
    * updated email
    
    * 'fout in main'
    
    * finished file upload
    
    * finished file upload

--- frontend/src/components/header/welcoming.tsx
+++ frontend/src/components/header/welcoming.tsx
@@ -1,36 +1,41 @@
 // possibly welcome doctor in the future ?
 import styles from '@/styles/PatientPage.module.css';
+import styleH from '@/styles/Header.module.css';
 
 interface GreetingDoctor {
-    doctorfirstname : string
-    doctorlastname : string
+  doctorfirstname: string;
+  doctorlastname: string;
 }
 
-export function WelcomingDoctor({doctorfirstname, doctorlastname}: GreetingDoctor) { 
-
-    const Welcoming = (doctor_name: string) => {
-        const welcome = "Welcome " + doctor_name 
-        return welcome
-    }
-    return (
+export function WelcomingDoctor({ doctorfirstname, doctorlastname }: GreetingDoctor) {
+  const Welcoming = ({ doctorfirstname, doctorlastname }: GreetingDoctor) => {
+    const welcome = 'Welcome Dr. '.concat(doctorfirstname, ' ', doctorlastname);
+    return welcome;
+  };
+  return (
     <>
-        <h2>{("Welcome Dr. ").concat(doctorfirstname, ' ', doctorlastname)}</h2>
-    </> )
-  }
-
+      <div className={styleH.welcoming}>{Welcoming({ doctorfirstname, doctorlastname })}</div>
+    </>
+  );
+}
 
 interface GreetingPatient {
-    patientfirstname : string 
-    patientlastname : string
-    doctorfirstname : string
-    doctorlastname : string
+  patientfirstname: string;
+  patientlastname: string;
+  doctorfirstname: string;
+  doctorlastname: string;
 }
 
-export function WelcomingPatient({patientfirstname, patientlastname, doctorfirstname, doctorlastname}: GreetingPatient){
-    return (
+export function WelcomingPatient({
+  patientfirstname,
+  patientlastname,
+  doctorfirstname,
+  doctorlastname,
+}: GreetingPatient) {
+  return (
     <>
-        <h3>{("Scans of patient ").concat(patientfirstname, ' ', patientlastname, '\n')}</h3>
-        <h4>{("under supervision of Dr. ").concat(doctorfirstname, " ", doctorlastname)} </h4>
+      <h3>{'Scans of patient '.concat(patientfirstname, ' ', patientlastname, '\n')}</h3>
+      <h4>{'under supervision of Dr. '.concat(doctorfirstname, ' ', doctorlastname)} </h4>
     </>
-        )
+  );
 }
