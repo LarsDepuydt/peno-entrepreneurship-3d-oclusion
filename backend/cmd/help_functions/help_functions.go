b002a4cfeb37f23cc5cc8b2c5f3360a4a5c1b1a7
LarsDepuydt <47790106+LarsDepuydt@users.noreply.github.com>
2023-03-28 14:28:02 +0200

    Fix database connection (#44)
    Fix database connection!

--- backend/cmd/help_functions/help_functions.go
+++ backend/cmd/help_functions/help_functions.go
@@ -2,21 +2,10 @@ package help_functions
 
 import (
 	"database/sql"
-	"fmt"
 
 	threedoclusionv1 "github.com/LarsDepuydt/peno-entrepreneurship-3d-oclusion/gen/proto/threedoclusion/v1"
 	_ "github.com/lib/pq"
 )
-
-func ConnectToDataBase() (*sql.DB, error) {
-	database, error := sql.Open("postgres", "host=host.docker.internal port=5430 user=docker password=docker1 dbname=patient_server sslmode=disable")
-	fmt.Println("Connection succesfull")
-	if error != nil {
-		return nil, error
-	}
-	return database, nil
-}
-
 
 func GetResponseMakerScan(rows *sql.Rows) ([]*threedoclusionv1.Scan, error) {
 	var rowArray []*threedoclusionv1.Scan
