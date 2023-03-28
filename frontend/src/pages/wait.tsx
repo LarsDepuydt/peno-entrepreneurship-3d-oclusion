fd912f841191152a861166edf40850e07477a883
kjell mesotten <kjell.mesotten@student.kuleuven.be>
2023-03-28 15:58:35 +0200

    new waiting page, slightly prettier

--- frontend/src/pages/wait.tsx
+++ frontend/src/pages/wait.tsx
@@ -4,10 +4,15 @@ import { ScanService } from "@/gen/proto/threedoclusion/v1/service_connect";
 import { createPromiseClient } from "@bufbuild/connect";
 import { createConnectTransport } from "@bufbuild/connect-web";
 import { useRouter } from 'next/router';
+import { useState } from "react";
 
 import styles from "@/styles/WaitPage.module.css"
+//import styles from "@/styles/globals.css"
 
 export default function WaitPage() {
+  const [submitted, setSubmitted] = useState(false);
+  const [formVisible, setFormVisible] = useState(true); // Add formVisible state
+
   const transport = createConnectTransport({
     baseUrl: "http://0.0.0.0:8080",
   });
@@ -35,6 +40,8 @@ export default function WaitPage() {
 
     if (submitOK) { // Trigger waiting procedure
       waitForResponse(codeValue);
+      setSubmitted(true); // Set submitted state to true
+      setFormVisible(false); // Set formVisible to false
     }
   }
   // See _app, can't use queryClient for streams so I made a new client here -> implement in _app as well to support other streams?
@@ -54,18 +61,32 @@ export default function WaitPage() {
 
   return (
     <div>
-    <div>
-    <h1>Waiting Page</h1>
-    <form onSubmit={afterSubmit}>
-      Code: <input type="number" id="code" size={20} name="code"/><br/>
-      <input type="submit" value="Submit"/> 
-    </form>
-    </div>
-    <div className={styles.spinner}>
-    <div className={styles.bounce1}></div>
-    <div className={styles.bounce2}></div>
-    <div className={styles.bounce3}></div>
-    </div>
+      {formVisible && ( // Display the form only if formVisible is true
+        <div>
+          <h1>Waiting Page</h1>
+          <form onSubmit={afterSubmit}>
+            Code: <input type="number" id="code" size={20} name="code"/><br/>
+            <input type="submit" value="Submit"/> 
+          </form>
+        </div>
+      )}
+      {submitted && ( // Display the following divs only if submitted is true
+        <div className={styles.bodah}>
+          <div className={styles.loading}>
+            <div className={styles.dot}></div>
+            <div className={styles.dot}></div>
+            <div className={styles.dot}></div>
+            <div className={styles.dot}></div>
+            <div className={styles.dot}></div>
+          </div>
+        </div>
+      )}
     </div>
   )
 }
+
+/*  <div className={styles.spinner}>
+    <div className={styles.bounce1}></div>
+    <div className={styles.bounce2}></div>
+    <div className={styles.bounce3}></div>
+    </div> */
