a1b784718513ddbb276aa6c45dbeb7657b887777
ArneVanDamme <arne@van-damme.eu>
2023-04-25 16:48:36 +0200

    wip

--- frontend/src/pages/settings.tsx
+++ frontend/src/pages/settings.tsx
@@ -2,7 +2,7 @@ import { useRouter } from 'next/router';
 import stylesButton from '@/styles/Buttons.module.css';
 import stylesText from '@/styles/Header.module.css';
 
-export default function settings() {
+export default function Settings() {
   const router = useRouter();
   const home = () => router.push('/patient');
   return (
