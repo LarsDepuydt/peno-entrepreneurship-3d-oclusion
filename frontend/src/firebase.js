fb5089a85a791998d6edb96b23492c3a8faacdf5
DagMalstaf <92030435+DagMalstaf@users.noreply.github.com>
2023-04-25 09:33:50 +0200

    fixing frontend

--- frontend/src/firebase.js
+++ frontend/src/firebase.js
@@ -1,19 +1,24 @@
+// Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getStorage } from "firebase/storage";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
 
-
-
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyAGEzsRDkqn0hsSWlEAJjRLKzu3kEZJ4GQ",
-  authDomain: "relu-vr-scan-database.firebaseapp.com",
-  projectId: "relu-vr-scan-database",
-  storageBucket: "relu-vr-scan-database.appspot.com",
-  messagingSenderId: "102837943574",
-  appId: "1:102837943574:web:87a1c484e33f82275be437",
-  measurementId: "G-GM8P0TDCZ6"
+  apiKey: "AIzaSyBnCIzVDVMlv4YyG0N9YTRHdhlQ_GQ18b8",
+  authDomain: "relu-backend.firebaseapp.com",
+  projectId: "relu-backend",
+  storageBucket: "relu-backend.appspot.com",
+  messagingSenderId: "309843565343",
+  appId: "1:309843565343:web:ba09eabcc3b6e98217ee2a",
+  measurementId: "G-XBYJT660Z4"
 };
 
+// Initialize Firebase
 const app = initializeApp(firebaseConfig);
 const storage = getStorage(app);
 
+// Export the instances you need
 export { storage };
