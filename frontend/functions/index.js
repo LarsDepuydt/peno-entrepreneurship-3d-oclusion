398bd62129aa40e16fb46ded65e469af0b74b8a9
DagMalstaf <92030435+DagMalstaf@users.noreply.github.com>
2023-04-25 17:48:41 +0200

    save - error

--- frontend/functions/index.js
+++ frontend/functions/index.js
@@ -2,7 +2,7 @@ const functions = require("firebase-functions");
 const next = require("next");
 
 const dev = process.env.NODE_ENV !== "production";
-const app = next({ dev, conf: { distDir: "../src/.next" } });
+const app = next({ dev, conf: { distDir: "../.next" } });
 const handle = app.getRequestHandler();
 
 exports.nextApp = functions.https.onRequest((req, res) => {
