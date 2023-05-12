4a566fb03392a7da2a609339f90cf1eba3c93dcd
ArneVanDamme <arne@van-damme.eu>
2023-05-12 14:36:14 +0200

    endVRCSS

--- frontend/src/pages/end-vr.tsx
+++ frontend/src/pages/end-vr.tsx
@@ -1,36 +1,23 @@
-import Link from 'next/link'
+import Link from 'next/link';
+import styleB from '@/styles/Buttons.module.css';
+import styleVR from '@/styles/EndVR.module.css';
+import stylesText from '@/styles/Header.module.css';
+import reluLogo from '../../public/relu-logo-small.png';
+import Image from 'next/image';
+import { useRouter } from 'next/router';
 
-export default function EndVRPage(){ 
-
-    // Redirected to on VR side when session has ended 
-    return (
-        <div>
-          <div className="container">
-          <div className="message">The scan has been saved successfully. You can exit safely now by closing the window.</div>
-            <button>
-            <Link href="/wait">Home</Link>
-            </button>
-          </div>
-          <style jsx>{`
-          .container {
-              display: flex;
-              flex-direction: column;
-              justify-content: center;
-              align-items: center;
-              height: 80%;
-              width: 80%;
-              padding: 2rem;
-              border-radius: 1rem;
-              background-color: #f3f3f3;
-            }
-            .message {
-              display: flex;
-              justify-content: center;
-              align-items: center;
-              font-size: 1.5rem;
-              margin-top: 2rem;
-            }
-          `}</style>
-        </div>
-      );
+export default function EndVRPage() {
+  // Redirected to on VR side when session has ended
+  const router = useRouter();
+  const home = () => router.push('/wait');
+  return (
+    <div>
+      <Image className={stylesText.small_logo_log} src={reluLogo} alt="relu logo" />
+      <div className={styleVR.massageWrapper}>
+        <div className={styleVR.text1}>The scan has been saved successfully.</div>
+        <div className={styleVR.text2}>You can now exit safely by closing the window.</div>
+        <button className={styleVR.button} id={styleB.homeIcon} onClick={home}></button>
+      </div>
+    </div>
+  );
 }
