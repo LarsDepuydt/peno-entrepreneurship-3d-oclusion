356db633c57b13a9cb9669f3e3813f3468234635
Jonas De Schouwer <jonasdeschouwer@gmail.com>
2023-05-12 14:21:38 +0200

    findSepAxis now sends tasks to workers

--- vr/src/cannon_collision/findSepAxis.js
+++ vr/src/cannon_collision/findSepAxis.js
@@ -1,16 +1,102 @@
 import * as CANNON from 'cannon-es';
 
+const NUM_WORKERS = 3;
+const WORKERS = new Array();
+let collect_id = 0;
+let worker_id = 0;
+
+// initialize WORKERS
+// give each worker an extra attribute 'collectors' which is a dict {collID: Collector}
+let worker;
+for (let i=0; i<NUM_WORKERS; i++) {
+    worker = new Worker('worker.js');
+    worker.onmessage = workerOnMessage;
+    worker.collectors = new Object();
+    WORKERS[i] = worker;
+}
+
+class Collector {
+    collID;             // unique identifier of the collector
+    numExpected;        // the number of expected results
+    numAcquired = 0;    // the number of acquired results
+    dmin = Number.MAX_VALUE;
+    vector;             // CANNON.Vec3
+    finished = false;   // true if the result of findSepAxis is known
+
+    constructor(coll_id, num_expected) {
+        this.id = coll_id;
+        this.vector = new CANNON.Vec3();
+        this.numExpected = num_expected;
+
+        WORKERS.forEach((worker) => {
+            worker.collectors[this.collID] = this;
+        });
+    }
+
+    sendTask(vector, posA, quatA, posB, quatB) {
+        worker = WORKERS[worker_id];
+        worker_id = (worker_id+1)%NUM_WORKERS;
+        worker.postMessage([this.collID, posA, quatA, posB, quatB, vector]);
+    }
+}
+
+
+// a web worker that implements the testSepAxis function asynchronously
+// note that no locks must be used as the worker receiver objects are not multithreaded
+function workerOnMessage(event) {
+    console.log(event.data);
+    const collID = event.data[0];
+    const d = event.data[1];
+    const collector = this.collectors[collID];
+
+    if (d==false) {
+        collector.dmin = false;
+        collector.finished = true;
+    } else if (d < collector.dmin) {
+        collector.dmin = d;
+        collector.vector = event.data[2];
+    }
+
+    collector.numAcquired += 1;
+    if (collector.numAcquired == collector.numExpected) {
+        collector.finished = true;
+    }
+}
+
+
+// hullA = this in order to overload
+function findSepAxisOverload(hullB, posA, quatA, posB, quatB, target, faceListA, faceListB) {
+    return findSepAxis(this, hullB, posA, quatA, posB, quatB, target, faceListA, faceListB);
+}
+
 // alternative to cannon.js function findSeparatingAxis that uses web workers
-function findSepAxis(hullB, posA, quatA, posB, quatB, target, faceListA, faceListB) {
+// return false if a separation axis is found, true otherwise
+// the separating axis is saved in target
+function findSepAxis(hullA, hullB, posA, quatA, posB, quatB, target, faceListA, faceListB) {
     const faceANormalWS3 = new CANNON.Vec3();
     const Worldnormal1 = new CANNON.Vec3();
     const deltaC = new CANNON.Vec3();
     const worldEdge0 = new CANNON.Vec3();
     const worldEdge1 = new CANNON.Vec3();
     const Cross = new CANNON.Vec3();
-    let dmin = Number.MAX_VALUE;
-    const hullA = this;
     let curPlaneTests = 0;
+
+    // calculate number of expected results
+    let num_expected = hullA.uniqueEdges.length * hullB.uniqueEdges.length;
+    if (!hullA.uniqueAxes) {
+        num_expected += numFacesA;
+    } else {
+        num_expected += hullA.uniqueAxes.length;
+    }
+    if (!hullB.uniqueAxes) {
+        num_expected += numFacesB;
+    } else {
+        num_expected += hullB.uniqueAxes.length;
+    }
+    const collID = collect_id;
+    const collector = new Collector(collID, num_expected);
+    collect_id += 1;
+
     if (!hullA.uniqueAxes) {
         const numFacesA = faceListA ? faceListA.length : hullA.faces.length;
         // Test face normals from hullA
@@ -19,14 +105,7 @@ function findSepAxis(hullB, posA, quatA, posB, quatB, target, faceListA, faceLis
             // Get world face normal
             faceANormalWS3.copy(hullA.faceNormals[fi]);
             quatA.vmult(faceANormalWS3, faceANormalWS3);
-            const d = hullA.testSepAxis(faceANormalWS3, hullB, posA, quatA, posB, quatB);
-            if (d === false) {
-                return false;
-            }
-            if (d < dmin) {
-                dmin = d;
-                target.copy(faceANormalWS3);
-            }
+            collector.sendTask(faceANormalWS3, posA, quatA, posB, quatB);
         }
     }
     else {
@@ -34,14 +113,7 @@ function findSepAxis(hullB, posA, quatA, posB, quatB, target, faceListA, faceLis
         for (let i = 0; i !== hullA.uniqueAxes.length; i++) {
             // Get world axis
             quatA.vmult(hullA.uniqueAxes[i], faceANormalWS3);
-            const d = hullA.testSepAxis(faceANormalWS3, hullB, posA, quatA, posB, quatB);
-            if (d === false) {
-                return false;
-            }
-            if (d < dmin) {
-                dmin = d;
-                target.copy(faceANormalWS3);
-            }
+            collector.sendTask(faceANormalWS3, posA, quatA, posB, quatB);
         }
     }
     if (!hullB.uniqueAxes) {
@@ -52,14 +124,7 @@ function findSepAxis(hullB, posA, quatA, posB, quatB, target, faceListA, faceLis
             Worldnormal1.copy(hullB.faceNormals[fi]);
             quatB.vmult(Worldnormal1, Worldnormal1);
             curPlaneTests++;
-            const d = hullA.testSepAxis(Worldnormal1, hullB, posA, quatA, posB, quatB);
-            if (d === false) {
-                return false;
-            }
-            if (d < dmin) {
-                dmin = d;
-                target.copy(Worldnormal1);
-            }
+            collector.sendTask(Worldnormal1, posA, quatA, posB, quatB);
         }
     }
     else {
@@ -67,14 +132,7 @@ function findSepAxis(hullB, posA, quatA, posB, quatB, target, faceListA, faceLis
         for (let i = 0; i !== hullB.uniqueAxes.length; i++) {
             quatB.vmult(hullB.uniqueAxes[i], Worldnormal1);
             curPlaneTests++;
-            const d = hullA.testSepAxis(Worldnormal1, hullB, posA, quatA, posB, quatB);
-            if (d === false) {
-                return false;
-            }
-            if (d < dmin) {
-                dmin = d;
-                target.copy(Worldnormal1);
-            }
+            collector.sendTask(Worldnormal1, posA, quatA, posB, quatB);
         }
     }
     // Test edges
@@ -87,17 +145,17 @@ function findSepAxis(hullB, posA, quatA, posB, quatB, target, faceListA, faceLis
             worldEdge0.cross(worldEdge1, Cross);
             if (!Cross.almostZero()) {
                 Cross.normalize();
-                const dist = hullA.testSepAxis(Cross, hullB, posA, quatA, posB, quatB);
-                if (dist === false) {
-                    return false;
-                }
-                if (dist < dmin) {
-                    dmin = dist;
-                    target.copy(Cross);
-                }
+                collector.sendTask(Cross, posA, quatA, posB, quatB);
             }
         }
     }
+
+    while (!collector.finished) {
+        console.log("waiting for collector to finish");
+    }
+    dmin = collector.dmin;
+    target = collector.vector;
+
     posB.vsub(posA, deltaC);
     if (deltaC.dot(target) > 0.0) {
         target.negate(target);
@@ -105,4 +163,4 @@ function findSepAxis(hullB, posA, quatA, posB, quatB, target, faceListA, faceLis
     return true;
 }
 
-export { findSepAxis };
+export { findSepAxis, findSepAxisOverload };
--- vr/src/cannon_collision/worker.js
+++ vr/src/cannon_collision/worker.js
@@ -2,13 +2,14 @@
 This worker will implement the function testSepAxis in a separate thread in order to improve performance
 
 Main->worker format:
-[posA: Vec3, quatA: Quaternion, posB: Vec3, quatB: Quaternion]
+[collID, posA: Vec3, quatA: Quaternion, posB: Vec3, quatB: Quaternion, vector: Vec3]
 
 Worker->main format:
-- number (wanneer overlap)
+- [collID, number, vector: Vec3] (wanneer overlap)
 of
-- false (wanneer geen overlap)
+- [collID, false] (wanneer geen overlap)
 */
+
 
 
 self.addEventListener("message", handleMessage);
@@ -16,10 +17,12 @@ self.addEventListener("message", handleMessage);
 
 
 function handleMessage(event) {
-    const posA = event.data[0];
-    const quatA = event.data[1];
-    const posB = event.data[2];
-    const quatB = event.data[3];
+    const collID = event.data[0];
+    const posA = event.data[1];
+    const quatA = event.data[2];
+    const posB = event.data[3];
+    const quatB = event.data[4];
+    const vector = event.data[5];
 
-    console.log(posA);
+    postMessage([collID, false]);
 }
