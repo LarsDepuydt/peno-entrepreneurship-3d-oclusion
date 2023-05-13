baecb3304b96a26f528e1d756b0f51da809fd711
senneve <66268408+senneve@users.noreply.github.com>
2023-05-13 23:04:49 +0200

    Component doesn't get rendered twice anymore

--- frontend/src/pages/video.tsx
+++ frontend/src/pages/video.tsx
@@ -1,10 +1,12 @@
-import { useState } from 'react';
+import { useState, memo } from 'react';
 import BeforeAfter from '@/components/vr/before-after';
 import VideoDownload from '@/components/vr/videodownload';
 import stylesVideo from '@/styles/Video.module.css';
 
 export default function VideoPage() {
   const [videoChunks, setVideoChunks] = useState([]);
+  const BeforeAfterMemo = memo(BeforeAfter); // So no rerender
+
 
   const handleVideoChunks = (chunks: any) => {
     setVideoChunks(chunks);
@@ -14,7 +16,7 @@ export default function VideoPage() {
     <div className={stylesVideo.video_page}>
       <div className={stylesVideo.container}>
         <div className={stylesVideo.before_after_container}>
-          <BeforeAfter onVideoChunksChange={handleVideoChunks} />
+          <BeforeAfterMemo onVideoChunksChange={handleVideoChunks} />
         </div>
         {videoChunks.length > 0 ? (
           <div className={stylesVideo.download_container}>
