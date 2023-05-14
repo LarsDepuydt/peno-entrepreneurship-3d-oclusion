f8a72f3dd1f1d777cf13bbce307b44640478c0b6
ArneVanDamme <arne@van-damme.eu>
2023-05-14 13:45:58 +0200

    zoom fix

--- frontend/src/components/vr/legenda.tsx
+++ frontend/src/components/vr/legenda.tsx
@@ -11,33 +11,35 @@ export function Legenda() {
         </p>
         <p className={stylesVR.legenda_list_item}>X</p>
         <p className={stylesVR.legenda_list_item}>Y</p>
+        <p className={stylesVR.legenda_list_item}>Squeeze</p>
         <p id={stylesVR.legenda_list_item_underline} className={stylesVR.legenda_list_item}>
           Right:
         </p>
         <p className={stylesVR.legenda_list_item}>A</p>
         <p className={stylesVR.legenda_list_item}>B</p>
+        <p className={stylesVR.legenda_list_item}>Squeeze</p>
         <p id={stylesVR.legenda_list_item_underline} className={stylesVR.legenda_list_item}>
           Both:
         </p>
         <p className={stylesVR.legenda_list_item}>Select</p>
-        <p className={stylesVR.legenda_list_item}>Squeeze</p>
       </div>
       <div className={stylesVR.legenda_list}>
         <p id={stylesVR.legenda_list_item_spacer} className={stylesVR.legenda_list_item}>
           {'.'}
         </p>
-        <p className={stylesVR.legenda_list_item}>Lock Axis</p>
+        <p className={stylesVR.legenda_list_item}>Lock axis</p>
         <p className={stylesVR.legenda_list_item}>Menu</p>
+        <p className={stylesVR.legenda_list_item}>Zoom in</p>
         <p id={stylesVR.legenda_list_item_spacer} className={stylesVR.legenda_list_item}>
           {'.'}
         </p>
         <p className={stylesVR.legenda_list_item}>Undo</p>
         <p className={stylesVR.legenda_list_item}>Redo</p>
+        <p className={stylesVR.legenda_list_item}>Zoom out</p>
         <p id={stylesVR.legenda_list_item_spacer} className={stylesVR.legenda_list_item}>
           {'.'}
         </p>
         <p className={stylesVR.legenda_list_item}>Hold to drag</p>
-        <p className={stylesVR.legenda_list_item}>Hold to zoom</p>
       </div>
     </div>
   );
