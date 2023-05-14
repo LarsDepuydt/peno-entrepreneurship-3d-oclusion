d8e3e41eb7ab1c391f33477d2a8bd689cd6b20d3
ArneVanDamme <92046132+ArneVanDamme@users.noreply.github.com>
2023-05-14 20:41:59 +0200

    Frontend/fixmenu (#161)
    * addedCSSfile
    
    * moreCSS
    
    * CSS
    
    * added icons
    
    * CSS
    
    * legenda
    
    * zoom fix
    
    * Removed legenda from menu
    
    ---------
    
    Co-authored-by: senneve <66268408+senneve@users.noreply.github.com>

---
+++ frontend/src/components/vr/legenda.tsx
@@ -0,0 +1,46 @@
+import { Canvas } from '@react-three/fiber';
+import { Html, Plane } from '@react-three/drei';
+import stylesVR from '@/styles/MenuVR.module.css';
+
+export function Legenda() {
+  return (
+    <div className={stylesVR.legenda}>
+      <div className={stylesVR.legenda_list}>
+        <p id={stylesVR.legenda_list_item_underline} className={stylesVR.legenda_list_item}>
+          Left:
+        </p>
+        <p className={stylesVR.legenda_list_item}>X</p>
+        <p className={stylesVR.legenda_list_item}>Y</p>
+        <p className={stylesVR.legenda_list_item}>Squeeze</p>
+        <p id={stylesVR.legenda_list_item_underline} className={stylesVR.legenda_list_item}>
+          Right:
+        </p>
+        <p className={stylesVR.legenda_list_item}>A</p>
+        <p className={stylesVR.legenda_list_item}>B</p>
+        <p className={stylesVR.legenda_list_item}>Squeeze</p>
+        <p id={stylesVR.legenda_list_item_underline} className={stylesVR.legenda_list_item}>
+          Both:
+        </p>
+        <p className={stylesVR.legenda_list_item}>Select</p>
+      </div>
+      <div className={stylesVR.legenda_list}>
+        <p id={stylesVR.legenda_list_item_spacer} className={stylesVR.legenda_list_item}>
+          {'.'}
+        </p>
+        <p className={stylesVR.legenda_list_item}>Lock axis</p>
+        <p className={stylesVR.legenda_list_item}>Menu</p>
+        <p className={stylesVR.legenda_list_item}>Zoom in</p>
+        <p id={stylesVR.legenda_list_item_spacer} className={stylesVR.legenda_list_item}>
+          {'.'}
+        </p>
+        <p className={stylesVR.legenda_list_item}>Undo</p>
+        <p className={stylesVR.legenda_list_item}>Redo</p>
+        <p className={stylesVR.legenda_list_item}>Zoom out</p>
+        <p id={stylesVR.legenda_list_item_spacer} className={stylesVR.legenda_list_item}>
+          {'.'}
+        </p>
+        <p className={stylesVR.legenda_list_item}>Hold to drag</p>
+      </div>
+    </div>
+  );
+}
